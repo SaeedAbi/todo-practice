271cc151fdecaf9dddda51e637301eefc17f75d4
SaeedAbi <abdary.saeed@gmail.com>
2024-07-28 16:49:34 +0330

    :lipstick: add TaskArea styles

---
+++ src/components/taskArea/TaskArea.tsx
@@ -0,0 +1,28 @@
+import React, {FC, ReactElement} from 'react';
+import {Box, Grid} from "@mui/material";
+import {format} from "date-fns";
+
+const TaskArea:FC = ():ReactElement => {
+    return (
+        <Grid item md={8} px={4} >
+            <Box mb={8} px={4}>
+            <h2>Status of your Tasks as on {format(new Date(),'PPPP') }</h2>
+            </Box>
+            <Grid container display='flex' justifyContent='center'>
+                <Grid item display='flex' flexDirection='row' justifyContent='space-around' alignItems='center'
+                md={10} xs={12} mb={8}
+                >
+                    <Box>Task Counter</Box>
+                    <Box>Task Counter</Box>
+                    <Box>Task Counter</Box>
+                </Grid>
+                <Grid item display='flex' flexDirection='column' xs={10} md={8}>
+                    <Box>Tasks will come over here</Box>
+                    <Box>Tasks will come over here</Box>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default TaskArea;
