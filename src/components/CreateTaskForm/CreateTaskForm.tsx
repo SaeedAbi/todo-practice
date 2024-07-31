f7f4a976eac81e45b39f1666f23bebc12ce21854
SaeedAbi <abdary.saeed@gmail.com>
2024-07-31 15:28:16 +0330

    :zap: create states with forms

--- src/components/CreateTaskForm/CreateTaskForm.tsx
+++ src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactElement} from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import {Box, Stack, Typography} from "@mui/material";
 import TaskTitle from "./_TaskTitle";
 import TaskDescription from "./_TaskDescription";
@@ -8,15 +8,24 @@ import {Status} from "./enums/Status";
 import {Priority} from "./enums/Priority";
 
 const CreateTaskForm:FC = ():ReactElement => {
+    const [title,setTitle]=useState<string|undefined>(undefined)
+    const [description,setDescription]=useState<string|undefined>(undefined)
+    const [date,setDate]=useState<Date|null>(new Date())
+    const [status,setStatus]=useState<string>(Status.todo)
+    const [priority,setPriority]=useState<string>(Priority.normal)
+
     return (
        <Box display='flex' flexDirection='column' alignItems='flex-start' width='100%' px={4} my={6}>
            <Typography component='h2' variant='h6' mb={2}>Create A Task</Typography>
            <Stack spacing={2} sx={{width:'100%'}}>
-            <TaskTitle/>
-           <TaskDescription/>
-               <TaskDate/>
+            <TaskTitle onChange={(e)=>setTitle(e.target.value)}/>
+           <TaskDescription onChange={e=>setDescription(e.target.value)}/>
+               <TaskDate onChange={date=>setDate(date)} value={date}/>
                <Stack direction='row' spacing={2} sx={{width:'100%'}}>
-               <TaskSelect label='Status' name='status' items={[
+               <TaskSelect label='Status' name='status'
+                           value={status}
+                           onChange={e=>setStatus(e.target.value as string)}
+                           items={[
                    {value:Status.todo,
                    label:Status.todo.toUpperCase(),
                    },
@@ -27,7 +36,10 @@ const CreateTaskForm:FC = ():ReactElement => {
                     label:Status.completed.toUpperCase(),
                    },
                ]} />
-               <TaskSelect label='Priority' name='priority' items={[
+               <TaskSelect label='Priority' name='priority'
+                           value={priority}
+                           onChange={e=>setPriority(e.target.value as string)}
+                           items={[
                    {
                        value:Priority.low,
                        label:Priority.low.toUpperCase(),
